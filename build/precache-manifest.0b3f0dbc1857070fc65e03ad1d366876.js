1a3e138b9dd917bea49cd41bfbf04b7a72748772
calumbutler <calum@gigantum.com>
2018-11-20 09:33:48 -0500

    removed build from gitignore, and created bundle

---
+++ build/precache-manifest.0b3f0dbc1857070fc65e03ad1d366876.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdb667cc85d93ab1c174",
+    "url": "/static/css/main.be0a84f7.chunk.css"
+  },
+  {
+    "revision": "cdb667cc85d93ab1c174",
+    "url": "/static/js/main.cdb667cc.chunk.js"
+  },
+  {
+    "revision": "182281b96e4353e82c2e",
+    "url": "/static/js/1.182281b9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb97409826218fb4c9dc03283ac4d41a",
+    "url": "/static/media/OldStandard-Regular.cb974098.ttf"
+  },
+  {
+    "revision": "11976ea17eb0dc9c8377f6549f4a2432",
+    "url": "/static/media/OldStandard-Bold.11976ea1.ttf"
+  },
+  {
+    "revision": "6b56f5551b9f7ca256fd87698dc3d34b",
+    "url": "/static/media/OldStandard-Italic.6b56f555.ttf"
+  },
+  {
+    "revision": "263db6bbef9d88965b23ece692bb46ed",
+    "url": "/static/media/WeddingInvitePaint.263db6bb.png"
+  },
+  {
+    "revision": "286b66ee2e7e01d1294d76a1206994ec",
+    "url": "/static/media/rothe_house.286b66ee.jpg"
+  },
+  {
+    "revision": "a498369de92b9b6dddab81f2d8ad1996",
+    "url": "/static/media/cleers.a498369d.jpg"
+  },
+  {
+    "revision": "1dc7e4928a473e6642180709b20b2c95",
+    "url": "/static/media/anocht.1dc7e492.jpg"
+  },
+  {
+    "revision": "7cca259bdd92bd0bd542c3488e23ff62",
+    "url": "/static/media/butlerhouse.7cca259b.jpg"
+  },
+  {
+    "revision": "b1c2264fbe94ac01b2b3f04be75369e1",
+    "url": "/static/media/cleeres.b1c2264f.jpg"
+  },
+  {
+    "revision": "27bb28b0041960fedde07cd04147ab1d",
+    "url": "/static/media/kilkennycastle.27bb28b0.jpg"
+  },
+  {
+    "revision": "a6d5b918798de9b90c27d8a3b47c1ce4",
+    "url": "/static/media/canices.a6d5b918.jpg"
+  },
+  {
+    "revision": "008b7d3f00d43d35b2d36e4b4d0f2327",
+    "url": "/static/media/woodstock.008b7d3f.jpg"
+  },
+  {
+    "revision": "9bb0d66fea4cc62c930c94af7b284b74",
+    "url": "/static/media/ardmore.9bb0d66f.jpg"
+  },
+  {
+    "revision": "bab8254d311bbb33937b44864e716aba",
+    "url": "/index.html"
+  }
+];
