b2520c2645793e830565e1be891c2704586c583b
calumbutler <calum@gigantum.com>
2018-11-24 17:38:05 -0500

    created a build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce6ad52dd0a601caa4ed817db0e64afd.js"
+  "/precache-manifest.322087b79d209675a4eb618fd01f58de.js"
 );
 
 workbox.clientsClaim();
