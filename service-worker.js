c974cc7ea8d7c409d4f46b33d47c8e4637c12ef4
calumbutler <calum@gigantum.com>
2018-11-24 17:48:17 -0500

    updated build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.322087b79d209675a4eb618fd01f58de.js"
+  "/precache-manifest.f9278eee223e1832e1e9e8fc5bf9c7fe.js"
 );
 
 workbox.clientsClaim();
